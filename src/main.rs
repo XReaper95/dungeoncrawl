d03871ff563729d736da38086de53dfa302db4ee
luisdt <luis.ernesto951008@gmail.com>
2022-06-28 11:57:40 -0300

    Basic bracket-lib structure

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Hello Bracket World!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().build()?;
+
+    main_loop(context, State {})
+}
